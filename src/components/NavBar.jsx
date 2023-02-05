5f8a7f8dc5fb4b244d328035cf105ec1c883fdc8
Nicos-08 <thierrylemoine136@gmail.com>
2023-02-05 10:45:17 +0100

    NavBar responsive

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,8 +1,20 @@
 import choixpeauLogo from '../assets/logo-choixpeau-magique.png';
 import "../styles/NavBar.css"
-
+import { useState, useEffect } from "react"
 
 function NavBar({ pageActuelle, updatePageActuelle }) {
+
+    const [width, setWidth] = useState(window.innerWidth);
+
+    useEffect(() => {
+      const handleWindowResize = () => {
+        setWidth(window.innerWidth);
+      };
+      window.addEventListener('resize', handleWindowResize);
+      return () => {
+        window.removeEventListener('resize', handleWindowResize);
+      };
+    }, []);
 
     const action = (onglet) => {
         updatePageActuelle(onglet);
@@ -15,25 +27,47 @@ function NavBar({ pageActuelle, updatePageActuelle }) {
     const onglet2 = "Choixpeau";
     const onglet3 = "Contact";
 
-    return(
-        <div className="navBar flex">
-            <img src={choixpeauLogo} alt="" title='Le choixpeau magique' className='choixpeauLogo' onClick={() => updatePageActuelle(onglet1)} />
-            <div className='navLinks'>
-                <ul className='flex'>
-                    <li className="accueilTab flex" title={onglet1} onClick={() => action(onglet1) } >
-                        <h4><i className="fa-solid fa-house icons"></i>{onglet1}</h4>
-                    </li>
-                    <li className="choixpeauTab flex" title={onglet2} onClick={() => action(onglet2)}>
-                        <h4><i className="fa-solid fa-hat-wizard"></i>{onglet2}</h4>
-                    </li>
-                    <li className="contactTab flex" title={onglet3} onClick={() => action(onglet3)}>
-                        <h4><i className="fa-solid fa-envelope icons"></i>{onglet3}</h4>
-                    </li>
-                </ul>
+    if(width > 710) {
+        return(
+            <div className="navBar flex">
+                <img src={choixpeauLogo} alt="" title='Le choixpeau magique' className='choixpeauLogo' onClick={() => updatePageActuelle(onglet1)} />
+                <div className='navLinks'>
+                    <ul className='flex'>
+                        <li className="accueilTab flex" title={onglet1} onClick={() => action(onglet1) } >
+                            <h4><i className="fa-solid fa-house icons"></i>{onglet1}</h4>
+                        </li>
+                        <li className="choixpeauTab flex" title={onglet2} onClick={() => action(onglet2)}>
+                            <h4><i className="fa-solid fa-hat-wizard"></i>{onglet2}</h4>
+                        </li>
+                        <li className="contactTab flex" title={onglet3} onClick={() => action(onglet3)}>
+                            <h4><i className="fa-solid fa-envelope icons"></i>{onglet3}</h4>
+                        </li>
+                    </ul>
+                </div>
             </div>
-        </div>
-    )
-
+        )
+    } else {
+        return (
+            <div className="navBar flex">
+                <div className="navLinks">
+                    <ul className="flex">
+                        <li className="accueilTab flex"title={onglet1} onClick={() => action(onglet1) } >
+                            <i className="fa-solid fa-house icons"></i>
+                            <h4>{onglet1}</h4>
+                        </li>
+                        <li className="choixpeauTab flex" title={onglet2} onClick={() => action(onglet2)}>
+                            <i className="fa-solid fa-hat-wizard"></i>
+                            <h4>{onglet2}</h4>
+                        </li>
+                        <li className="contactTab flex" title={onglet3} onClick={() => action(onglet3)}>
+                            <i className="fa-solid fa-envelope icons"></i>
+                            <h4>{onglet3}</h4>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        )
+    }
 }
 
 export default NavBar
