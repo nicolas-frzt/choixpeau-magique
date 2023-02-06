906be40b2b58248c719ba8d729ee429b271126fe
Nicos-08 <thierrylemoine136@gmail.com>
2023-02-06 15:47:08 +0100

    Célébrités ajoutées

--- src/data/maisons-data.js
+++ src/data/maisons-data.js
@@ -11,20 +11,20 @@ export const maisonsData = [
         historique: "La maison Gryffondor est l'une des quatre maisons de l'école de Poudlard dans la série Harry Potter. Elle a été fondée par Godric Gryffondor, un des quatre fondateurs de l'école. La maison est réputée pour ses qualités de courage, de loyauté, de détermination et de force d'âme. Les élèves de Gryffondor sont souvent considérés comme des leader naturels et des combattants pour la justice. Les membres de cette maison ont également une forte tendance à l'amitié et à la camaraderie. Les couleurs de la maison sont le rouge et or, et leur emblème est un lion. Les étudiants de Gryffondor ont tendance à être respectés et admirés par les autres maisons de Poudlard, en particulier pour leur courage et leur détermination. C'est souvent la maison qui a le plus de membres dans l'armée de Dumbledore contre les mangemorts. C'est également la maison dans laquelle Harry Potter, Ron Weasley et Hermione Granger ont été classés.",
         celebrites: [
             {
-                nom: "",
-                photo: ""
+                nom: "Godric Gryffondor",
+                photo: "https://zupimages.net/up/23/06/vcmn.png"
             },
             {
-                nom: "",
-                photo: ""
+                nom: "Albus Dumbledore",
+                photo: "https://getwallpapers.com/wallpaper/full/9/0/b/714371-free-download-albus-dumbledore-wallpapers-2560x1600-for-4k.jpg"
             },
             {
-                nom: "",
-                photo: ""
+                nom: "Remus Lupin",
+                photo: "https://imgix.bustle.com/rehost/2016/9/25/d791bd9c-030f-4516-9b1c-7951e72b8c23.jpg?w=800&fit=crop&crop=faces&auto=format%2Ccompress"
             },
             {
-                nom: "",
-                photo: ""
+                nom: "Harry Potter",
+                photo: "https://img.ecartelera.com/noticias/43200/43218-m.jpg"
             }
         ]
     },
@@ -36,20 +36,20 @@ export const maisonsData = [
         historique: `La maison Serpentard est l'une des quatre maisons de l'école de sorcellerie Poudlard, fondée par Salazar Serpentard. Les élèves qui y sont affectés sont généralement considérés comme intelligents, rusés et ambitieux. La maison est associée à des couleurs vert et argent, et son emblème est un serpent. Les élèves de Serpentard sont souvent perçus comme étant manipulateurs et calculateurs, mais ils sont également reconnus pour leur intelligence et leur ambition. Au fil des années, les élèves de Serpentard ont été considérés comme les plus talentueux et les plus ambitieux de Poudlard, et ils ont réussi à remporter de nombreux prix et distinctions. Ils ont également eu leur part de conflits avec les autres maisons, en particulier Gryffondor et Poufsouffle. La maison Serpentard a également été associée à des personnages maléfiques dans l'histoire de Poudlard, mais cela ne reflète pas la véritable nature de tous les élèves qui y ont été affectés. Il est important de se rappeler que chaque élève est unique et que les actions de quelques-uns ne reflètent pas celles de tous les membres de la maison.`,
         celebrites: [
             {
-                nom: "",
-                photo: ""
+                nom: "Salazar Serpentard",
+                photo: "https://zupimages.net/up/23/06/bfi6.png"
             },
             {
-                nom: "",
-                photo: ""
+                nom: "Lucius Malefoy",
+                photo: "https://www.nextplz.fr/wp-content/uploads/nextplz/2020/04/lucius-malefoy---jason-isaacs-e1587533656262.jpg"
             },
             {
-                nom: "",
-                photo: ""
+                nom: "Severus Rogue",
+                photo: "https://yt3.ggpht.com/a/AATXAJw9jTzK43uel8vqAGf1x-AcSqhn46JlKJCXXA=s900-c-k-c0xffffffff-no-rj-mo"
             },
             {
-                nom: "",
-                photo: ""
+                nom: "Regulus Black",
+                photo: "http://pm1.narvii.com/6253/dfa5aac2e38abe65405e98431cb8d3de31d58ad2_00.jpg"
             }
         ]
     },
@@ -62,20 +62,20 @@ export const maisonsData = [
         La devise de Serdaigle est "Intelligence, curiosité, découverte" reflétant les valeurs de la maison qui valorise la soif de connaissances, la curiosité intellectuelle et la recherche de la vérité. Les élèves qui y sont affectés sont souvent considérés comme des érudits et des passionnés de savoir, ils ont tendance à être des élèves modèles et à réussir brillamment dans les matières telles que les études sur les moldus, l'histoire de la magie, la divination et les runes. Les membres de cette maison sont souvent des leaders naturels, ils sont indépendants et cherchent à comprendre les choses par eux-mêmes.`,
         celebrites: [
             {
-                nom: "",
-                photo: ""
+                nom: "Rowena Serdaigle",
+                photo: "https://zupimages.net/up/23/06/n8yu.png"
             },
             {
-                nom: "",
-                photo: ""
+                nom: "Filius Flitwick",
+                photo: "https://zupimages.net/up/23/06/urik.jpg"
             },
             {
-                nom: "",
-                photo: ""
+                nom: "Gilderoy Lockhart",
+                photo: "https://1.bp.blogspot.com/-WrZEeGNWCBY/Xny3eWC316I/AAAAAAAANgo/4kcgk9yM-OUf6vkRlk69pWTxOuG3aVi7QCLcBGAsYHQ/s1600/Gilderoy%2BLockhart%2B%2528III%2529.jpg"
             },
             {
-                nom: "",
-                photo: ""
+                nom: "Sibylle Trelawney",
+                photo: "https://vignette.wikia.nocookie.net/doblaje/images/c/c5/HSybillTrelawney.jpg/revision/latest?cb=20160125013704&path-prefix=es"
             }
         ]
     },
@@ -91,20 +91,20 @@ export const maisonsData = [
         Poufsouffle a également produit des sorciers célèbres tels que Cedric Diggory, Nymphadora Tonks et Newt Scamander. Les membres de Poufsouffle sont souvent considérés comme des élèves travailleurs et sérieux, mais aussi comme des amis loyaux et des alliés fiables. Il est considéré comme l'équivalent de la maison Hufflepuff dans la série Harry Potter.`,
         celebrites: [
             {
-                nom: "",
-                photo: ""
+                nom: "Helga Poufsouffle",
+                photo: "https://zupimages.net/up/23/06/vlqd.jpg"
             },
             {
-                nom: "",
-                photo: ""
+                nom: "Pomona Chourave",
+                photo: "https://fontan.io/media/www.warnerbros.fr/1/6/16aa97e5cad336ef913ea9d5feaed3ff.jpg"
             },
             {
-                nom: "",
-                photo: ""
+                nom: "Norbert Dragonneau",
+                photo: "https://zupimages.net/up/23/06/dolh.jpg"
             },
             {
-                nom: "",
-                photo: ""
+                nom: "Nymphadora Tonks",
+                photo: "http://orig11.deviantart.net/5575/f/2013/197/c/2/nymphadora_tonks_render_by_ninfadorabile-d6dr3zy.png"
             }
         ]
     },
